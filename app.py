b697daeece2721a45f59c6797fba4555e520400f
SUMANTH M <133026914+Sum4n7h19@users.noreply.github.com>
2024-08-09 18:57:34 +0530

    port added

--- app.py
+++ app.py
@@ -788,4 +788,4 @@ api.add_resource(Watershed, '/')
 port = int(os.environ.get("PORT", 5000))
 # Main Driver Function
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host ="http://127.0.0.1", port=port, debug=True)
